8f5a1fc391e43b3665bedcfd962a353e55c14d23
Pattrick Hueper <phueper@hueper.net>
2009-10-11 01:26:59 +0200

    add edited event handler
    Signed-off-by: Pattrick Hueper <phueper@hueper.net>

--- scala/src/SkypeConnector.scala
+++ scala/src/SkypeConnector.scala
@@ -7,11 +7,13 @@ class SkypeConnector {
   // converter for anonymous func to ChatMessageListener
   implicit def func2ChatMessageListener(funcs:(
     (ChatMessage)=>Unit,	/* received func */
-    (ChatMessage)=>Unit		/* sent func */
+    (ChatMessage)=>Unit,	/* sent func */
+    (ChatMessage)=>Unit		/* edited func */
   	)) = {
       new ChatMessageListener() {
         def chatMessageReceived(m:ChatMessage) = funcs._1.apply(m)
         def chatMessageSent(m:ChatMessage) = funcs._2.apply(m)
+        def chatMessageEdited(m:ChatMessage) = funcs._3.apply(m)
       }
   }
   def connect {
@@ -20,7 +22,8 @@ class SkypeConnector {
     Skype.setDebug(true)
     Skype.addChatMessageListener((
       (m:ChatMessage) => { println("message received from" + m.getSenderDisplayName + ": " + m.getContent) },
-      (m:ChatMessage) => { println("message sent: " + m.getContent) }
+      (m:ChatMessage) => { println("message sent: " + m.getContent)},
+      (m:ChatMessage) => { println("message edited: " + m.getContent) }
     ))
     
   }
