9ebd26f6725b7867ea57075848857fe2598a7cb1
Pattrick Hueper <phueper@hueper.net>
2009-10-11 22:27:59 +0200

    attempt to fix indentation...
    Signed-off-by: Pattrick Hueper <phueper@hueper.net>

--- scala/src/BackgroundMsgHandler.scala
+++ scala/src/BackgroundMsgHandler.scala
@@ -10,25 +10,25 @@ object BackgroundMsgHandler extends Actor {
       receive {
         case m:ChatMessage => {
           m.getStatus match {
-		      case ChatMessage.Status.RECEIVED => {
-		        println("message received from" + m.getSenderDisplayName)
-		        m.getContent match {
-		          case _ => println(m.getContent)
-		        }
-		      }
-		      case ChatMessage.Status.SENT => { println("message sent...")
-		    	var rP = new Regex(""".*will (.*) spielen.*""");
-                m.getContent match {
-		          case rP(what) => {
-		            m.getChat.send("Aber ich hab keine Lust " + what + " zu spielen")
-		          }
-		          case "xyzzy" => {
-		        	  m.getChat.send("xyzzy?")
-		          }
-		          case _ => println(m.getContent)
-		        }
-		      }
-		    }
+            case ChatMessage.Status.RECEIVED => {
+              println("message received from" + m.getSenderDisplayName)
+              m.getContent match {
+                case _ => println(m.getContent)
+              }
+            }
+            case ChatMessage.Status.SENT => { println("message sent...")
+              var rP = new Regex(""".*will (.*) spielen.*""");
+              m.getContent match {
+                case rP(what) => {
+                  m.getChat.send("Aber ich hab keine Lust " + what + " zu spielen")
+                }
+                case "xyzzy" => {
+                  m.getChat.send("xyzzy?")
+                }
+                case _ => println(m.getContent)
+              }
+            }
+          }
         }
       }
     }
